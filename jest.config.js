7ae7484f6cb3668edaa950b35375763182ddb5fa
rajasekhar1205-cloud <rajasekharking1205@gmail.com>
2026-03-27 09:01:21 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: 'jest-expo',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
